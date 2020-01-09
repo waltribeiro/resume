fe3ed97826aceb6b8660014b32d9de7032d43d2b
WaltRibeiro <walt@fororchestra.com>
2020-01-09 08:55:12 -0500

    updated images folder and projects tabs

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ function pulse{
     document.getElementById("linkedin").innerHTML = "rotate("+degrees+"deg)";
 }
 */
+
+function myFunction() {
+    document.getElementById("certificatesPopUp").innerHTML = "Hello World";
+  }
