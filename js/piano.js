0e776e75cdd66ded561eaf796d62aa39a9fe163f
WaltRibeiro <walt@fororchestra.com>
2020-02-03 10:07:36 -0500

    added dog piano;2C

--- js/piano.js
+++ js/piano.js
@@ -1,12 +1,15 @@
 const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
 const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
 
-const keys = document.querySelectorAll('.key')
+const keys = document.querySelectorAll('.key') // array of divs with class query
 const whiteKeys = document.querySelectorAll('.key.white')
 const blackKeys = document.querySelectorAll('.key.black')
 
 keys.forEach(key => {
-  key.addEventListener('click', () => playNote(key))
+  key.addEventListener('click', () => {
+    playNote(key);
+    displayNoteLetter(key);
+  })
 })
 
 document.addEventListener('keydown', e => {
@@ -28,3 +31,27 @@ function playNote(key) {
     key.classList.remove('active')
   })
 }
+
+function displayNoteLetter (keyDiv) {
+  
+
+
+}
+
+
+
+// this is the dog piano function
+function myFunction() {
+  var x = document.getElementById("myP").innerHTML;
+  document.getElementById("demo").innerHTML = x;
+}
+
+// build an array of all possible values
+// match the dfata attributes from the HTML
+// when clicked
+//    check to see if the value is correct
+//    go from 0 => correct array array.length
+//    within the forLoop {if getElementById.getAttribute("data-note") == the current i value in the array
+// then it's the correct note}
+
+//domains and range in pus and oouts
