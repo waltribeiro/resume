6b02f4d19581ffc858203da32324431aedfb010b
WaltRibeiro <walt@fororchestra.com>
2020-02-15 22:30:15 -0500

    fixed link in header

--- js/freelancer.js
+++ js/freelancer.js
@@ -72,11 +72,14 @@
 var chosenWord = "art";
 
 
+
 function checkPass() {
-    var promptInput = prompt("Please enter the password to see more work");
-    if (promptInput === chosenWord) {
-        window.location.replace("http://linkedin.waltrib.com");
-    } else {
-        alert("Incorrect password")
-    }
-}
+  var promptInput = prompt("Please enter the password to see more work");
+  if (promptInput === chosenWord) {
+      window.location.replace("http://linkedin.waltrib.com");
+  } else if (promptInput != chosenWord) {
+      alert("Incorrect password")
+  } else {
+
+  }
+};
